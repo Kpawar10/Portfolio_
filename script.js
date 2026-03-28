6be7e886faf311038ce8486e8104b5cb27301285
Kritika Pawar <kritikapawar06@gmail.com>
2026-03-28 23:48:54 +0530

    updated

--- script.js
+++ script.js
@@ -83,3 +83,13 @@ window.addEventListener("load", () => {
     loader.classList.add("hidden");
   }, 2000);
 });
+document.querySelectorAll(".project-img").forEach((el) => {
+  el.addEventListener("mousemove", (e) => {
+    const rect = el.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    el.style.setProperty("--x", `${x}px`);
+    el.style.setProperty("--y", `${y}px`);
+  });
+});
